4f8f43d6909ab8d2f98e5ef5ed8a56454997bf15
tiexin.gong <tiexin.gong@qq.com>
2017-10-17 15:31:41 +0800

    修改eslint规则　去掉npm-watch 添加uuid 使用方法import uuid from 'uuid/v1';（时间戳）

--- src/components/common/Breadcrumb.js
+++ src/components/common/Breadcrumb.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import { Breadcrumb } from 'antd';
-import { utils } from 'hl-utils/lib/index';
+import uuid from 'uuid/v1';
 
 @inject('store')
 @observer
@@ -14,12 +14,12 @@ export default class HlBreadcrumb extends Component {
     const items = this.props.breadcrumb.map((item) => {
       if (item.path) {
         return (
-          <Breadcrumb.Item key={utils.randomString13()}>
+          <Breadcrumb.Item key={uuid}>
             <Link to={item.path}>{item.name}</Link>
           </Breadcrumb.Item>
         );
       }
-      return <Breadcrumb.Item key={utils.randomString13()}>{item.name}</Breadcrumb.Item>;
+      return <Breadcrumb.Item key={uuid}>{item.name}</Breadcrumb.Item>;
     });
 
     return (
--- src/components/common/form.js
+++ src/components/common/form.js
@@ -95,14 +95,13 @@ export function createFormItem(getFieldDecorator, config, key) {
       break;
 
     case 'uploadDragger':
-      control = getFieldDecorator(config.name, config.options)(
-        <Dragger {...props}>
+      control = getFieldDecorator(config.name, config.options)(<Dragger {...props}>
           <p className="ant-upload-drag-icon">
             <Icon type="inbox" />
           </p>
           <p className="ant-upload-text">点击或将文件拖拽到此区域上传</p>
           <p className="ant-upload-hint">支持单个或批量上传，严禁上传公司内部资料及其他违禁文件</p>
-        </Dragger>);
+                                                               </Dragger>);
       break;
 
     case 'button':
