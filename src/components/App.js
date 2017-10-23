0d504bc95540cdca9bf313ca93562ac18623ad4b
tiexin.gong <tiexin.gong@qq.com>
2017-10-23 10:01:04 +0800

    取消lazy-route

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { withRouter, Route } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
 import uuid from 'uuid/v4';
-import LazyRoute from 'lazy-route';
+// import LazyRoute from 'lazy-route';
 import routes from '../routes';
 
 @inject('store')
@@ -17,11 +17,11 @@ export default class App extends Component {
           <Route
             key={uuid()}
             path={path}
-            // component={route.component}
+            component={component}
             exact
-            render={props => (
-              <LazyRoute {...props} component={import(componentUrl)} />
-            )}
+            // render={props => (
+            //   <LazyRoute {...props} component={import(componentUrl)} />
+            // )}
           />
         ))}
       </div>
