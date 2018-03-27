8aad9f78816b10732e84615d6148c38dad209817
tiexin.gong <tiexin.gong@qq.com>
2018-03-27 17:20:56 +0800

    update

--- src/shared/components/App/App.js
+++ src/shared/components/App/App.js
@@ -1,37 +1,108 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { withRouter, Route } from "react-router-dom";
 import { inject, observer } from "mobx-react";
 import DevTools from "mobx-react-devtools";
-import { Card } from "antd";
+import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import "./app.less";
 import { routes } from "../../router/routes";
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
 
 @inject("store")
 @withRouter
 @observer
 export default class App extends Component {
+  state = {
+    collapsed: false,
+  };
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
   renderRoute = ({ path, component }) => (
     <Route key={path} path={path} component={component} exact />
   );
 
   render() {
     return (
-      <Fragment>
-        {/* 建议不写内链样式,这里偷懒 */}
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "350px",
-          }}
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
         >
-          <Card style={{ width: 300 }}>
-            <h2>前端项目模板</h2>
-          </Card>
-        </div>
-        <DevTools />
-        {routes.map(this.renderRoute)}
-      </Fragment>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={
+                <span>
+                  <Icon type="user" />
+                  <span>User</span>
+                </span>
+              }
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={
+                <span>
+                  <Icon type="team" />
+                  <span>Team</span>
+                </span>
+              }
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: "#fff", padding: 0 }} />
+          <Content
+            style={{
+              margin: "0 16px",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <Breadcrumb style={{ margin: "16px 0" }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div
+              style={{
+                padding: 24,
+                background: "#fff",
+                flex: 1,
+              }}
+            >
+              Bill is a cat.
+              <DevTools />
+              {routes.map(this.renderRoute)}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
