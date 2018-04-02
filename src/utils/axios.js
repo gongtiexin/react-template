4067a07a5e69728f0e9aa21211f29c3fff9a15a0
tiexin.gong <tiexin.gong@qq.com>
2018-04-02 16:49:10 +0800

    axios失败重试

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -7,31 +7,73 @@
 import axios from "axios";
 import { notification } from "antd";
 
-axios.default.timeout = 5000;
+axios.defaults.retry = 4;
+axios.defaults.retryDelay = 1000;
 
 /**
- * axios拦截器,处理401,403等等状态码
+ * axios拦截器
  */
 axios.interceptors.response.use(
   response => Promise.resolve(response),
   error => {
-    if (error.response) {
-      switch (error.response.status) {
-        case 401: {
-          // TODO
-          break;
+    const { config } = error;
+    // If config does not exist or the retry option is not set, reject
+    if (!config || !config.retry) return Promise.reject(error);
+
+    // Set the variable for keeping track of the retry count
+    config.retryCount = config.retryCount || 0;
+
+    // Check if we've maxed out the total number of retries
+    if (config.retryCount >= config.retry) {
+      if (error.response) {
+        switch (error.response.status) {
+          case 401: {
+            // TODO
+            break;
+          }
+          default:
+            break;
         }
-        case 403: {
-          notification.warn({ message: "您没有权限这样做!" });
-          break;
-        }
-        default:
-          break;
       }
+      // Reject with the error
+      return Promise.reject(error);
     }
-    return Promise.reject(error);
+
+    // Increase the retry count
+    config.retryCount += 1;
+
+    // Create new promise to handle exponential backoff
+    const backoff = new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, config.retryDelay || 1);
+    });
+
+    // Return the promise in which recalls axios to retry the request
+    return backoff.then(() => axios(config));
   }
 );
+
+// axios.interceptors.response.use(
+//   response => Promise.resolve(response),
+//   error => {
+//     if (error.response) {
+//       switch (error.response.status) {
+//         case 401: {
+//           // TODO
+//           break;
+//         }
+//         case 403: {
+//           notification.warn({ message: "您没有权限这样做!" });
+//           break;
+//         }
+//         default:
+//           break;
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
 
 /**
  * ajax请求同意封装
@@ -62,8 +104,24 @@ const request = (config, success, error) =>
     },
     ({ response }) => {
       const newConfig = {};
-      if (response && response.data && response.data.errMsg) {
-        newConfig.description = response.data.errMsg;
+      switch (response.status) {
+        case 403: {
+          newConfig.description = "您没有权限这样做";
+          break;
+        }
+        case 503: {
+          newConfig.description = "服务器当前无法处理请求";
+          break;
+        }
+        case 502: {
+          newConfig.description = "服务器接暂无响应";
+          break;
+        }
+        default: {
+          if (response && response.data && response.data.errMsg) {
+            newConfig.description = response.data.errMsg;
+          }
+        }
       }
       if (error && error.message) {
         newConfig.message = error.message;
