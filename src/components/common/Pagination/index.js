f86039e2c7c00cc269a7fa67a6ff4fd628b8a9d3
gongtiexin <tiexin.gong@qq.com>
2018-08-10 14:32:11 +0800

    search table
    Signed-off-by: gongtiexin <tiexin.gong@qq.com>

--- src/components/common/Pagination/index.js
+++ src/components/common/Pagination/index.js
@@ -8,48 +8,45 @@ import React, { Component } from "react";
 import { inject, observer } from "mobx-react";
 import { Pagination as AntdPagination } from "antd";
 import PropTypes from "prop-types";
+import "./index.less";
 
 @inject("store")
 @observer
 export default class Pagination extends Component {
   static propTypes = {
-    paginationProps: PropTypes.shape({
-      pageSize: PropTypes.number,
-      currentPage: PropTypes.number,
-      totalElements: PropTypes.number,
-      showSizeChanger: PropTypes.bool,
-      onChange: PropTypes.func,
-    }),
+    pageSize: PropTypes.number,
+    currentPage: PropTypes.number,
+    totalElements: PropTypes.number,
+    showSizeChanger: PropTypes.bool,
+    handleChange: PropTypes.func,
   };
 
   static defaultProps = {
-    paginationProps: {
-      pageSize: 10,
-      currentPage: 1,
-      totalElements: 0,
-      showSizeChanger: false,
-      onChange: (_page, _size) => console.log(_page, _size),
-    },
+    pageSize: 10,
+    currentPage: 1,
+    totalElements: 0,
+    showSizeChanger: false,
+    handleChange: (page, size) => console.log(`page:${page}, size:${size}`),
   };
 
   onShowSizeChange = (current, pageSize) => {
-    this.props.paginationProps.onChange(current, pageSize);
+    this.props.handleChange({ page: current, size: pageSize });
   };
 
   onChange = (pageNumber, pageSize) => {
-    this.props.paginationProps.onChange(pageNumber, pageSize);
+    this.props.handleChange({ page: pageNumber, size: pageSize });
   };
 
   showTotal = total => `共 ${total || 0} 条`;
 
   render() {
-    const { pageSize, currentPage, totalElements, showSizeChanger } = this.props.paginationProps;
+    const { pageSize, currentPage, totalElements, showSizeChanger } = this.props;
 
     return (
       <AntdPagination
         current={currentPage}
         pageSize={pageSize}
-        className="fe-float-right fe-margin-nomal fe-clear"
+        className="rs-pagination"
         showSizeChanger={showSizeChanger || false}
         onShowSizeChange={this.onShowSizeChange}
         showQuickJumper
---
+++ src/components/common/SearchTable/index.js
@@ -0,0 +1,117 @@
+/**
+ * Date              Author           Des
+ *----------------------------------------------
+ * 2018/8/10           gongtiexin       通用搜索表格组件
+ * */
+
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import PropTypes from "prop-types";
+import { Button, Col, Divider, Form, Input, Row, Table, Select } from "antd";
+import Pagination from "../Pagination";
+import "./index.less";
+
+const { Item: FormItem, create } = Form;
+const { Option: SelectOption } = Select;
+
+@create()
+@observer
+export default class SearchTable extends Component {
+  static propTypes = {
+    form: PropTypes.object.isRequired,
+    fields: PropTypes.array,
+    dividerProps: PropTypes.object,
+    tableProps: PropTypes.object,
+    paginationProps: PropTypes.object,
+    callback: PropTypes.func,
+  };
+
+  static defaultProps = {
+    fields: [],
+    callback: values => console.log(values),
+  };
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  handleSearch = e => {
+    e.preventDefault();
+    this.props.form.validateFields(() => {
+      this.refreshData();
+    });
+  };
+
+  handleReset = () => {
+    this.props.form.resetFields();
+  };
+
+  refreshData = params => {
+    const {
+      callback,
+      form: { getFieldsValue },
+    } = this.props;
+    const search = getFieldsValue();
+    callback({ ...search, ...params });
+  };
+
+  renderFormItem = fields =>
+    fields.map(({ type, key, label, items = [], options, props }) => {
+      const { getFieldDecorator } = this.props.form;
+      switch (type) {
+        case "input": {
+          return (
+            <Col span={8} key={key}>
+              <FormItem label={label}>
+                {getFieldDecorator(key, options)(<Input {...props} />)}
+              </FormItem>
+            </Col>
+          );
+        }
+        case "select": {
+          return (
+            <Col span={8} key={key}>
+              <FormItem label={label}>
+                {getFieldDecorator(key, options)(
+                  <Select {...props}>
+                    {items.map(item => (
+                      <SelectOption key={item.key || item.value} value={item.value}>
+                        {item.label}
+                      </SelectOption>
+                    ))}
+                  </Select>
+                )}
+              </FormItem>
+            </Col>
+          );
+        }
+        default:
+          return null;
+      }
+    });
+
+  render() {
+    const { fields, dividerProps, tableProps, paginationProps } = this.props;
+
+    return (
+      <div id="searchTable">
+        <Form className="ant-advanced-search-form" onSubmit={this.handleSearch}>
+          <Row gutter={24}>{this.renderFormItem(fields)}</Row>
+          <Row>
+            <Col span={24} style={{ textAlign: "right" }}>
+              <Button type="primary" htmlType="submit">
+                搜索
+              </Button>
+              <Button style={{ marginLeft: 8 }} onClick={this.handleReset}>
+                重置
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+        <Divider {...dividerProps}>搜索结果</Divider>
+        <Table size="middle" pagination={false} {...tableProps} />
+        <Pagination {...paginationProps} handleChange={this.refreshData} />
+      </div>
+    );
+  }
+}
