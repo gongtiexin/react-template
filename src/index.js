58aa185a72a8bedf76ca7a6c6bf3a8d3e7a5948b
gongtiexin <tiexin.gong@qq.com>
2019-07-17 16:47:42 +0800

    去掉不要的东西

--- src/index.js
+++ src/index.js
@@ -8,14 +8,22 @@ import 'regenerator-runtime/runtime';
 
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'mobx-react';
 import { hotRehydrate, rehydrate } from 'rfx-core';
 import moment from 'moment';
 import 'normalize.css';
+import Loadable from './components/common/Loadable';
 import { isProduction } from './utils/constants';
 import './stores';
-import './styles/main.less';
-import Xfyun from './components/demo/Xfyun';
+import './styles/global.less';
+
+/**
+ * 代码拆分和按需加载
+ */
+const LoadableApp = Loadable({
+  loader: () => import(/* webpackChunkName: "route-root" */ './components/App'),
+});
 
 /**
  * moment时区设置为中国
@@ -27,7 +35,11 @@ const store = rehydrate();
 const renderApp = () => {
   render(
     <Provider store={isProduction ? store : hotRehydrate()}>
-      <Xfyun />
+      <Router>
+        <Switch>
+          <Route path="/" component={LoadableApp} />
+        </Switch>
+      </Router>
     </Provider>,
     document.getElementById('root'),
   );
--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,12 @@
 import Loadable from '../components/common/Loadable';
 
 const routes = [
-  {
-    path: '/',
-    component: Loadable({
-      loader: () => import(/* webpackChunkName: "route-home" */ '../components/demo/Xfyun'),
-    }),
-  },
+  // {
+  //   path: '/',
+  //   component: Loadable({
+  //     loader: () => import(/* webpackChunkName: "route-home" */ '../components/demo/Xfyun'),
+  //   }),
+  // },
 ];
 
-const breadcrumbNameMap = {
-  '/route': 'label',
-};
-
-export { routes, breadcrumbNameMap };
+export { routes };
