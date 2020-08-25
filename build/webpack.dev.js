53fcf730747952c1895b9e9daf5bb74faab9531e
gongtiexin <tiexin.gong@qq.com>
2020-08-25 16:36:02 +0800

    build(sever): 端口在3000-3999动态获取

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,3 +1,4 @@
+const portfinder = require('portfinder');
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const apiMocker = require('mocker-api');
@@ -5,62 +6,81 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const common = require('./webpack.common.js');
 const path = require('./path');
 
-module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    devServer: {
-        hot: true,
-        contentBase: path.root,
-        port: 3001,
-        host: '0.0.0.0',
-        publicPath: '/',
-        historyApiFallback: true,
-        disableHostCheck: true,
-        quiet: true,
-        before(app) {
-            apiMocker(app, path.mock, {
-                changeHost: true
-            });
-        },
-        proxy: {
-            '/api': {
-                target: `https://unidemo.dcloud.net.cn`,
-                changeOrigin: true
-            }
-        }
-    },
-    output: {
-        filename: 'app.[hash].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less|css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'less-loader',
-                        options: {
-                            lessOptions: {
-                                javascriptEnabled: true
+module.exports = () =>
+    new Promise((resolve, reject) => {
+        portfinder
+            .getPortPromise({
+                port: 3000, // minimum port
+                stopPort: 3999 // maximum port
+            })
+            .then((port) => {
+                resolve(
+                    merge(common, {
+                        mode: 'development',
+                        devtool: 'eval-source-map',
+                        devServer: {
+                            hot: true,
+                            contentBase: path.root,
+                            port: port,
+                            host: '0.0.0.0',
+                            publicPath: '/',
+                            historyApiFallback: true,
+                            disableHostCheck: true,
+                            quiet: true,
+                            before(app) {
+                                apiMocker(app, path.mock, {
+                                    changeHost: true
+                                });
+                            },
+                            proxy: {
+                                '/api': {
+                                    target: `https://unidemo.dcloud.net.cn`,
+                                    changeOrigin: true
+                                }
                             }
-                        }
-                    },
-                    {
-                        loader: 'style-resources-loader',
-                        options: {
-                            patterns: path.lessVariables,
-                            injector: 'append'
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [new FriendlyErrorsWebpackPlugin(), new webpack.HotModuleReplacementPlugin()]
-});
+                        },
+                        output: {
+                            filename: 'app.[hash].js'
+                        },
+                        module: {
+                            rules: [
+                                {
+                                    test: /\.less|css$/,
+                                    use: [
+                                        {
+                                            loader: 'style-loader'
+                                        },
+                                        {
+                                            loader: 'css-loader'
+                                        },
+                                        {
+                                            loader: 'less-loader',
+                                            options: {
+                                                lessOptions: {
+                                                    javascriptEnabled: true
+                                                }
+                                            }
+                                        },
+                                        {
+                                            loader: 'style-resources-loader',
+                                            options: {
+                                                patterns: path.lessVariables,
+                                                injector: 'append'
+                                            }
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        plugins: [
+                            new FriendlyErrorsWebpackPlugin(),
+                            new webpack.HotModuleReplacementPlugin()
+                        ]
+                    })
+                );
+            })
+            .catch((err) => {
+                // Could not get a free port, `err` contains the reason.
+                console.log(err);
+            });
+    });
